408ffef10d3cedf8a169353277c99ee4a1806ce9
jakob.mueller <jakob.mueller@r210-206.schule.local>
2019-03-05 12:09:21 +0100

    Neue Klasse

---
+++ Übung zu kontrollstrukturen/src/RepositoryTest.java
@@ -0,0 +1,21 @@
+
+public class RepositoryTest {
+	
+	static int variable=4;
+	static int andereZahl;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//lustge_Methode();
+		
+	}
+	public static void lustige_Methode()
+	{
+		if(variable==4) 
+		{
+			 andereZahl= (variable+3) ;
+		}
+		
+		System.out.print(andereZahl);
+	}
+}
