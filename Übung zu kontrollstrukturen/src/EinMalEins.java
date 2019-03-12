a6c2f19399ca64f50e28f9f5233286e0ecec83db
jakob.mueller <jakob.mueller@r210-206.schule.local>
2019-03-12 11:55:32 +0100

    AB 9 Monatstage

--- Übung zu kontrollstrukturen/src/EinMalEins.java
+++ Übung zu kontrollstrukturen/src/EinMalEins.java
@@ -1,9 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class EinMalEins {
+	
+	static List<String> monate=new ArrayList<String>();
+	static List<Integer> monatstage=new ArrayList<Integer>();
 
+	static int tag=12;
+	static int monat=3;
+	static int jahr=2019;
+	static int tageSeit1900; 
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		einMalEins() ;
+		//einMalEins() ;
+		wochentage();
 	}
 
 		public static void einMalEins() {
@@ -13,5 +24,26 @@ public class EinMalEins {
 				}
 				System.out.println("");
 			}
+		}
+		
+		public static void wochentage() {
+
+			int zwischenergebnis= (jahr - 1900)/4;
+			
+			monatstage.add(31);
+			monatstage.add(28);
+			monatstage.add(31);
+			monatstage.add(30);
+			monatstage.add(31);
+			monatstage.add(30);
+			monatstage.add(31);
+			monatstage.add(31);
+			monatstage.add(33);
+			monatstage.add(31);
+			monatstage.add(30);
+			monatstage.add(31);
+			
+			tageSeit1900=( zwischenergebnis + (jahr - 1900)*365)+(monatstage.get(monat-3)+tag);
+			System.out.print(tageSeit1900);
 		}
 }
